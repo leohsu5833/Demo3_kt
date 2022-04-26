8134ddf6d987c47707e3f0d64bdfcd34f40da543
Steven Hsu <leohsu5833@gmail.com>
2022-04-26 11:11:56 +0800

    add go to second activity scenario

--- app/src/main/java/com/example/demo3_kt/MainActivity.kt
+++ app/src/main/java/com/example/demo3_kt/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.demo3_kt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun doActivity(v : View) {
+        val intent = Intent()
+        intent.setClass(this,SecondActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/demo3_kt/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.demo3_kt
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second)
+    }
+
+
+}
